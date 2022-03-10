7239a8e56157d0df987871107e07b6e58ff7692d
Aditya Thakur <18bce242@nirmauni.ac.in>
2022-03-10 23:52:28 +0530

    Home page cards implemented

--- src/components/Home/InvestmentDetails.js
+++ src/components/Home/InvestmentDetails.js
@@ -1,22 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import "./InvestmentDetails.css"
 
+const getCompanyIndex = (stocksData, company) => {
+    for(let i=0; i<stocksData.length; i++) {
+        if(stocksData[i].company === company) {
+            return i;
+        }
+    }
+}
+
 function InvestmentDetails(props) {
+
+    const [totalInvestment, setTotalInvestment] = useState(0);
+    const [currentTotal, setCurrentTotal] = useState(0);
+    const [profitLoss, setProfitLoss] = useState(0);
+
+    const profitLossStyle = parseFloat(profitLoss) < 0 ? "loss" : "profit";
+
+    useEffect(() => {
+        let totalInvestment = 0;
+        let currentTotal = 0;
+
+        for (var company of Object.keys(props.assets)) {
+            totalInvestment += parseFloat(props.assets[company].total);
+
+            const index = getCompanyIndex(props.stocksData, company);
+            const price = parseFloat(props.stocksData[index].ltp);
+            const units = parseFloat(props.assets[company].quantity);
+            currentTotal += price * units;
+        }
+        setTotalInvestment(totalInvestment.toFixed(2));
+
+        let profitLoss = currentTotal - totalInvestment;
+        profitLoss = profitLoss.toFixed(2);
+        currentTotal = currentTotal.toFixed(2);
+        setCurrentTotal(currentTotal);
+        setProfitLoss(profitLoss);
+
+    }, [props.stocksData, props.assets])
+
     return (
         <div className="investment-detail">
             <div className="investment-detail-top">
                 <div className="top-card">
                     <p className="top-card-text">Total Investment</p>
-                    <p className="top-card-value">1000.00</p>
+                    <p className="top-card-value">₹{totalInvestment}</p>
                 </div>
-                <div className="top-card">Current Total</div>
-                <div className="top-card">Profit/Loss</div>
-            </div>
-            <div className="investment-detail-bottom">
-                <div className="wallet-balance">
-                    <p>Wallet Balance</p>
-                    <p className="wallet-balance-amount">₹{props.walletBalance}</p>
+                <div className="top-card">
+                    <p className="top-card-text">Current Total</p>
+                    <p className="top-card-value">₹{currentTotal}</p>
+                </div>
+                <div className="top-card">
+                    <p className="top-card-text">{parseFloat(profitLoss) < 0 ? "Loss" : "Profit"}</p>
+                    <p className={"top-card-value " + profitLossStyle}>₹{Math.abs(parseFloat(profitLoss))}</p>
+                </div>
+                <div className="top-card">
+                    <p className="top-card-text">Wallet Balance</p>
+                    <p className="top-card-value">₹{props.walletBalance}</p>
                 </div>
             </div>
         </div>
@@ -26,6 +67,8 @@ function InvestmentDetails(props) {
 const mapStateToProps = (state) => {
 	return {
 		walletBalance: state.wallet.balance,
+        assets: state.assets,
+        stocksData: state.stockData.stocksData,
 	}
 }
 
